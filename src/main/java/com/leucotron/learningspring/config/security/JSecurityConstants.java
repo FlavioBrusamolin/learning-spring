5eff50bad7fd0f2b21a7923a571c60d0a9142e6c
FlavioBrusamolin <flaviobrusamolin@gec.inatel.br>
2020-01-27 09:06:47 -0300

    Remove values from properties

--- src/main/java/com/leucotron/learningspring/config/security/JSecurityConstants.java
+++ src/main/java/com/leucotron/learningspring/config/security/JSecurityConstants.java
@@ -1,7 +1,5 @@
 package com.leucotron.learningspring.config.security;
 
-import org.springframework.beans.factory.annotation.Value;
-
 /**
  *
  * @author flavio
@@ -10,12 +8,8 @@ public final class JSecurityConstants {
     
     public static final String AUTH_URL = "/api/v1/auth";
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_HEADER = "Authorization";
-    
-//    @Value("${security.jwt.secret}")
-    public static String JWT_SECRET = "learning_spring_secret_key";
-    
-//    @Value("${security.jwt.expiration}")
-    public static Integer JWT_EXPIRATION = 43200000;
+    public static final String TOKEN_HEADER = "Authorization";    
+    public static final String JWT_SECRET = "learning_spring_secret_key";    
+    public static final Integer JWT_EXPIRATION = 43200000;
     
 }
